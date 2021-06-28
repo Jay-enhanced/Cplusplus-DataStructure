9b098315df6808508873132e3af7bd59215d603c
Ashe <34436659+Ryzen7-3700X@users.noreply.github.com>
2021-06-28 17:46:04 +0800

    链表增加迭代器

--- MyDoubleLink.h
+++ MyDoubleLink.h
@@ -25,152 +25,261 @@ template<class _Ty>
 class MyDoubleLink
 {
 public:
-	MyDoubleLink() : count(0)
-	{
-		phead = new LinkNode<_Ty>();
-		phead->prev = phead;
-		phead->next = phead;
-	}
-	~MyDoubleLink()
-	{
-		LinkNode<_Ty>* ptmp;
-		LinkNode<_Ty>* pnode = phead->next;
+	using pNode = LinkNode<_Ty>*;
+	using Node  = LinkNode<_Ty>;
 
-		while (pnode != phead)
+	MyDoubleLink();
+	~MyDoubleLink();
+
+	// 1.返回链表的大小
+	unsigned int size() const;
+	// 2.返回链表是否为空
+	bool isEmpty() const;
+	// 3.头插
+	void insert_front(_Ty value);
+	// 4.尾插
+	void insert_tail(_Ty value);
+	// 5.在index前增加节点
+	void insert_at(const unsigned int& index, _Ty value);
+	// 6.删除第一个值为value的节点
+	void delete_node(_Ty value);
+	// 7.更新节点
+	void update_node(_Ty old_value, _Ty new_value);
+	// 8.查找index处的节点值
+	_Ty get(const unsigned int& index);
+	// 9.打印链表
+	void print();
+
+	// 内置迭代器
+	class DoubleLinkIterator
+	{
+	public:
+		DoubleLinkIterator(pNode p = nullptr):ptr(p){}
+		// 1.重载*运算符
+		Node& operator*() noexcept
 		{
-			ptmp = pnode;
-			pnode = pnode->next;
-			delete ptmp;
+			return *ptr;
+		}
+		// 2.重载->运算符
+		pNode operator->() noexcept
+		{
+			return ptr;
+		}
+		// 3.重载==运算符
+		bool operator==(const DoubleLinkIterator& n) noexcept
+		{
+			return this->ptr == n.ptr;
+		}
+		// 4.重载!=运算符
+		bool operator!=(const DoubleLinkIterator& n) noexcept
+		{
+			return this->ptr != n.ptr;
+		}
+		// 5.重载前置++运算符
+		DoubleLinkIterator& operator++() noexcept
+		{
+			ptr = ptr->next;
+			return *this;
+		}
+		// 6.重载后置++运算符
+		DoubleLinkIterator operator++(int) noexcept
+		{
+			pNode tmp = ptr;
+			ptr = ptr->next;
+			return DoubleLinkIterator(tmp);
+		}
+		// 7.重载前置--运算符
+		DoubleLinkIterator& operator--() noexcept
+		{
+			ptr = ptr->prev;
+			return *this;
+		}
+		// 8.重载后置--运算符
+		DoubleLinkIterator operator--(int) noexcept
+		{
+			pNode tmp = ptr;
+			ptr = ptr->prev;
+			return DoubleLinkIterator(tmp);
 		}
 
-		delete phead;
-		phead = nullptr;
-	}
+	private:
+		pNode ptr;
+	};
 
-	// 返回链表的大小
-	unsigned int size() const
+	using iterator = DoubleLinkIterator;
+
+	iterator begin()
 	{
-		return count;
+		return DoubleLinkIterator(phead->next);
 	}
-
-	// 返回链表是否为空
-	bool isEmpty() const
+	iterator end()
 	{
-		return count == 0;
-	}
-
-	// 在头部插入一个节点
-	void insert_front(_Ty value)
-	{
-		LinkNode<_Ty>* pnode = new LinkNode<_Ty>(value, phead, phead->next);
-		phead->next->prev = pnode;
-		phead->next = pnode;
-		count++;
-	}
-
-	// 在尾部插入一个节点
-	void insert_tail(_Ty value)
-	{
-		LinkNode<_Ty>* pnode = new LinkNode<_Ty>(value, phead->prev, phead);
-		phead->prev->next = pnode;
-		phead->prev = pnode;
-		count++;
-	}
-
-	// 在index前增加节点
-	void insert_at(const unsigned int& index, _Ty value)
-	{
-		if (index == 0)
-		{
-			insert_front(value);
-			return;
-		}
-
-		LinkNode<_Ty>* pnode_index = get_node(index);
-		LinkNode<_Ty>* pnode = new LinkNode<_Ty>(value, pnode_index->prev, pnode_index);
-		pnode_index->prev->next = pnode;
-		pnode_index->prev = pnode;
-		count++;
-	}
-
-	// 删除第一个值为value的节点
-	void delete_node(_Ty value)
-	{
-		if (isEmpty())
-		{
-			throw MyException("LinkList is empty.");
-		}
-
-		LinkNode<_Ty>* p = phead->next;
-		while (p != phead)
-		{
-			if (p->value == value)
-			{
-				p->prev->next = p->next;
-				p->next->prev = p->prev;
-				delete p;
-				count--;
-				return;
-			}
-			p = p->next;
-		}
-		throw MyException("The corresponding node was not found in the LinkList.");
-	}
-
-	// 通过传入旧值和新值修改一个节点
-	void update_node(_Ty old_value, _Ty new_value)
-	{
-		LinkNode<_Ty>* p = phead->next;
-
-		while (p != phead)
-		{
-			if (p->value == old_value)
-			{
-				p->value = new_value;
-				return;
-			}
-			p = p->next;
-		}
-		throw MyException("The corresponding node was not found in the LinkList.");
-	}
-
-	// 查找index处的节点值
-	_Ty get(const unsigned int& index)
-	{
-		LinkNode<_Ty>* node = get_node(index);
-		return node->value;
+		return DoubleLinkIterator(phead);
 	}
 
 private:
 	unsigned int count;
-	LinkNode<_Ty>* phead;
-	LinkNode<_Ty>* get_node(int index)
-	{
-		if (index < 0 || index > count - 1 || count == 0)
-		{
-			throw MyException("Index out of range.");
-		}
-
-		// 正向查找
-		if (index <= count / 2)
-		{
-			unsigned int i = 0;
-			LinkNode<_Ty>* p_index = phead->next;
-			while (i++ < index)
-			{
-				p_index = p_index->next;
-			}
-			return p_index;
-		}
-
-		// 反向查找
-		unsigned int i = 0;
-		unsigned int counts = count - index - 1;
-		LinkNode<_Ty>* r_index = phead->prev;
-		while (i++ < counts)
-		{
-			r_index = r_index->prev;
-		}
-		return r_index;
-	}
+	pNode phead;
+	LinkNode<_Ty>* get_node(int index);
 };
+
+template<class _Ty>
+MyDoubleLink<_Ty>::MyDoubleLink():count(0)
+{
+	phead = new LinkNode<_Ty>();
+	phead->prev = phead;
+	phead->next = phead;
+}
+
+template<class _Ty>
+MyDoubleLink<_Ty>::~MyDoubleLink()
+{
+	pNode ptmp;
+	pNode pnode = phead->next;
+
+	while (pnode != phead)
+	{
+		ptmp = pnode;
+		pnode = pnode->next;
+		delete ptmp;
+	}
+
+	delete phead;
+	phead = nullptr;
+}
+
+template<class _Ty>
+inline unsigned int MyDoubleLink<_Ty>::size() const
+{
+	return count;
+}
+
+template<class _Ty>
+inline bool MyDoubleLink<_Ty>::isEmpty() const
+{
+	return count == 0;
+}
+
+template<class _Ty>
+void MyDoubleLink<_Ty>::insert_front(_Ty value)
+{
+	pNode pnode = new Node(value, phead, phead->next);
+	phead->next->prev = pnode;
+	phead->next = pnode;
+	count++;
+}
+
+template<class _Ty>
+void MyDoubleLink<_Ty>::insert_tail(_Ty value)
+{
+	pNode pnode = new Node(value, phead->prev, phead);
+	phead->prev->next = pnode;
+	phead->prev = pnode;
+	count++;
+}
+
+template<class _Ty>
+void MyDoubleLink<_Ty>::insert_at(const unsigned int& index, _Ty value)
+{
+	if (index == 0)
+	{
+		insert_front(value);
+		return;
+	}
+
+	pNode pnode_index = get_node(index);
+	pNode pnode = new Node(value, pnode_index->prev, pnode_index);
+	pnode_index->prev->next = pnode;
+	pnode_index->prev = pnode;
+	count++;
+}
+
+template<class _Ty>
+void MyDoubleLink<_Ty>::delete_node(_Ty value)
+{
+	if (isEmpty())
+	{
+		throw MyException("LinkList is empty.");
+	}
+
+	pNode p = phead->next;
+	while (p != phead)
+	{
+		if (p->value == value)
+		{
+			p->prev->next = p->next;
+			p->next->prev = p->prev;
+			delete p;
+			count--;
+			return;
+		}
+		p = p->next;
+	}
+	throw MyException("The corresponding node was not found in the LinkList.");
+}
+
+template<class _Ty>
+void MyDoubleLink<_Ty>::update_node(_Ty old_value, _Ty new_value)
+{
+	pNode p = phead->next;
+
+	while (p != phead)
+	{
+		if (p->value == old_value)
+		{
+			p->value = new_value;
+			return;
+		}
+		p = p->next;
+	}
+	throw MyException("The corresponding node was not found in the LinkList.");
+}
+
+template<class _Ty>
+_Ty MyDoubleLink<_Ty>::get(const unsigned int& index)
+{
+	pNode node = get_node(index);
+	return node->value;
+}
+
+template<class _Ty>
+void MyDoubleLink<_Ty>::print()
+{
+	pNode tmp = phead;
+	while (tmp && tmp->next != phead)
+	{
+		cout << tmp->next->value << " ";
+		tmp = tmp->next;
+	}
+}
+
+template<class _Ty>
+inline LinkNode<_Ty>* MyDoubleLink<_Ty>::get_node(int index)
+{
+	if (index < 0 || index > count - 1 || count == 0)
+	{
+		throw MyException("Index out of range.");
+	}
+
+	// 正向查找
+	if (index <= count / 2)
+	{
+		unsigned int i = 0;
+		LinkNode<_Ty>* p_index = phead->next;
+		while (i++ < index)
+		{
+			p_index = p_index->next;
+		}
+		return p_index;
+	}
+
+	// 反向查找
+	unsigned int i = 0;
+	unsigned int counts = count - index - 1;
+	LinkNode<_Ty>* r_index = phead->prev;
+	while (i++ < counts)
+	{
+		r_index = r_index->prev;
+	}
+	return r_index;
+}
